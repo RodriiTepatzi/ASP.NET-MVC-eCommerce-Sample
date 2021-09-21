7932aa8539545a82c3dd4eccce4c9ecf5f08826d
RodriiTepatzi <72983645+RodriiTepatzi@users.noreply.github.com>
2021-09-21 15:13:07 -0500

    Implementing all features in EntityBaseRepository

--- Data/Services/IActorsService.cs
+++ Data/Services/IActorsService.cs
@@ -1,4 +1,5 @@
-﻿using eCommerceTest.Models;
+﻿using eCommerceTest.Data.Base;
+using eCommerceTest.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,12 +7,8 @@ using System.Threading.Tasks;
 
 namespace eCommerceTest.Data.Services
 {
-    public interface IActorsService
+    public interface IActorsService : IEntityBaseRepository<Actor>
     {
-        Task<IEnumerable<Actor>> GetAllAsync();
-        Task<Actor> GetByIdAsync(int id);
-        Task AddAsync(Actor actor);
-        Task<Actor> UpdateAsync(int id, Actor newActor);
-        Task DeleteAsync(int id);
+        
     }
 }
--- Models/Actor.cs
+++ Models/Actor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eCommerceTest.Data.Base;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace eCommerceTest.Models
 {
-    public class Actor
+    public class Actor : IEntityBase
     {
         [Key]
         public int Id { get; set; }
