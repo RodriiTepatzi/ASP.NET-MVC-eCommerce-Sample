dde016073991c2d4226fe2614cce676b5e3e7074
RodriiTepatzi <72983645+RodriiTepatzi@users.noreply.github.com>
2021-09-30 23:53:24 -0500

    Creating "Create" View for Movies

---
+++ Data/ViewModels/NewMovieVM.cs
@@ -0,0 +1,57 @@
+﻿using eCommerceTest.Data;
+using eCommerceTest.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceTest.Models
+{
+    public class NewMovieVM
+    {
+        [Display(Name = "Movie Name")]
+        [Required(ErrorMessage = "Movie name is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Movie Description")]
+        [Required(ErrorMessage = "Movie description is required")]
+        public string Description { get; set; }
+
+        [Display(Name = "Price")]
+        [Required(ErrorMessage = "Movie price is required")]
+        public double Price { get; set; }
+
+        [Display(Name = "Image")]
+        [Required(ErrorMessage = "Movie image is required")]
+        public string Image { get; set; }
+
+        [Display(Name = "Start Date")]
+        [Required(ErrorMessage = "Start date is required")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End Date")]
+        [Required(ErrorMessage = "End date is required")]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Category")]
+        [Required(ErrorMessage = "Category is required")]
+        public MovieCategory MovieCategory { get; set; }
+
+        //Relationship
+
+        [Display(Name = "Select actor(s)")]
+        [Required(ErrorMessage = "Movie actor(s) is required")]
+        public List<int> ActorIds { get; set; }
+
+        [Display(Name = "Select a cinema")]
+        [Required(ErrorMessage = "Cinema is required")]
+        public int CinemaId { get; set; }
+
+        [Display(Name = "Select a producer")]
+        [Required(ErrorMessage = "Producer is required")]
+        public int ProducerId { get; set; }
+
+    }
+}
